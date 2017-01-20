b54cfeb1252e83d4e97834f8103303f6ad69dca9
Joey Kudish <joey@jkudish.com>
2017-01-19 16:20:42 -0800

    refactor the registration to work with Laravel 5.4

--- src/TailServiceProvider.php
+++ src/TailServiceProvider.php
@@ -32,13 +32,11 @@ class TailServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app['command.tail'] = $this->app->singleton(
-            function ($app) {
-                return new TailCommand();
-            }
-        );
-
-        $this->commands('command.tail');
+         if ($this->app->runningInConsole()) {
+            $this->commands([
+                TailCommand::class,
+            ]);
+        }
     }
 
     /**
