10920b906269dd5f930e1683fd9be5712bf6196c
Freek Van der Herten <freek@spatie.be>
2017-03-16 22:15:09 +0000

    Apply fixes from StyleCI

--- src/TailCommand.php
+++ src/TailCommand.php
@@ -74,17 +74,17 @@ class TailCommand extends Command
     protected function tailRemoteLogFile($connection)
     {
         $connectionParameters = config('tail.connections.'.$connection);
-        
+
         // set default port to 22 or use the port
         // from config file if `port` is defined.
         $port = 22;
-        if (!empty($connectionParameters['port'])) {
-        	$port = $connectionParameters['port'];
+        if (! empty($connectionParameters['port'])) {
+            $port = $connectionParameters['port'];
         }
 
         $this->guardAgainstInvalidConnectionParameters($connectionParameters);
 
-        $tailCommand = 'ssh '.($connectionParameters['user'] == '' ? '' : $connectionParameters['user'].'@').$connectionParameters['host']." -p " . $port . " -T 'cd ".$connectionParameters['logDirectory'].';tail -n '.$this->option('lines')." -f $(ls -t | head -n 1)'";
+        $tailCommand = 'ssh '.($connectionParameters['user'] == '' ? '' : $connectionParameters['user'].'@').$connectionParameters['host'].' -p '.$port." -T 'cd ".$connectionParameters['logDirectory'].';tail -n '.$this->option('lines')." -f $(ls -t | head -n 1)'";
 
         $this->info('start tailing latest remote log on host '.$connectionParameters['host'].' (port '.$port.') in directory '.$connectionParameters['logDirectory']);
 
--- src/config/tail.php
+++ src/config/tail.php
@@ -27,7 +27,7 @@ return [
             /*
              * The port of remote ssh
              */
-            'port' => '22'
+            'port' => '22',
         ],
     ],
 ];
