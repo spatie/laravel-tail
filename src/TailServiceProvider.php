6d1a78aafcd41ff8cfe1efdf1527213041c0b956
freek <freek@spatie.be>
2020-05-15 23:43:46 +0200

    wip

--- src/TailServiceProvider.php
+++ src/TailServiceProvider.php
@@ -8,19 +8,20 @@ class TailServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__.'/../config/tail.php' => config_path('tail.php'),
-        ], 'tail-config');
-    }
-
-    public function register()
-    {
         if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/tail.php' => config_path('tail.php'),
+            ], 'tail-config');
+
+
             $this->commands([
                 TailCommand::class,
             ]);
         }
+    }
 
-        $this->mergeConfigFrom(__DIR__.'/../config/tail.php', 'tail');
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/tail.php', 'tail');
     }
 }
